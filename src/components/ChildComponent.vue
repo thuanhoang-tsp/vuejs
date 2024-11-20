c1520372e355f3a7a0280c6a77106672e5b9afa4
thuanhoang-tsp <thuan.hoang@tsp.com.vn>
2024-11-20 16:48:26 +0700

    for loop array

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,35 +1,18 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 
-type User = {
-    name: string;
-    age: number;
-    status: 'active' | 'deactive';
-    loading: boolean
-}
-
-const props = defineProps<{
-    user: User,
-    handleChangeStatus: (status: 'active' | 'deactive') => void
-}>()
+const tasks = ref([
+    { id: 1, name: 'Task 1' },
+    { id: 2, name: 'Task 2' },
+    { id: 3, name: 'Task 3' },
+    { id: 4, name: 'Task 4' },
+    { id: 5, name: 'Task 5' },
+])
 
 </script>
 
 <template>
-    <div>
-        <div v-if="props.user.loading">Loading .... </div>
-        <div v-else>
-            <div v-if="props.user.status === 'active'">
-                <div>name: <span style="font-weight: bold;">{{ props.user.name }}</span></div>
-                <div>age: <span style="font-weight: bold;">{{ props.user.age }}</span></div>
-            </div>
-            <div v-if="props.user.status === 'deactive'">
-                User is blocked
-            </div>
-        </div>
-    </div>
-
-    <div>
-        <button @click="handleChangeStatus('active')" :disabled="user.status === 'active'">Active</button>
-        <button @click="handleChangeStatus('deactive')" :disabled="user.status === 'deactive'">Deactive</button>
+    <div v-for="(task, index) in tasks" :key="index">
+        {{ task.name }}
     </div>
 </template>
